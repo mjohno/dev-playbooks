9f0bba2bbe35d58b20f7aacd4e2a28954914b55e
mjohno <mj1ohno7@gmail.com>
2021-07-04 11:20:45 +0200

    Adding invoke tasks for selecting the mitmproxy mode

--- tasks.py
+++ tasks.py
@@ -13,3 +13,20 @@ def install(c, tags=None):
             "ansible-playbook -i inventory -K playbooks/bootstrap.yml",
             pty=True
         )
+
+@task
+def mitmproxy(c, mode='regular'):
+    if mode == 'regular':
+        c.run(
+            f"ansible-playbook -i inventory --tags mitmproxy "
+            f"-K -e mitm_proxy_mode={mode} -e mitm_state=unchanged "
+            "playbooks/bootstrap.yml",
+            pty=True
+        )
+    elif mode == 'transparent':
+        c.run(
+            f"ansible-playbook -i inventory --tags mitmproxy "
+            f"-K -e mitm_proxy_mode={mode} -e mitm_state=unchanged "
+            "playbooks/bootstrap.yml",
+            pty=True
+        )
